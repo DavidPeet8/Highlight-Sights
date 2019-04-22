722810e4ecab63c0cba5fc85df8fa66657308e81
David Peet <davidpeetp@gmail.com>
2019-04-22 13:34:54 -0400

    inital commit

---
+++ content.js
@@ -0,0 +1,42 @@
+// Author: David Peet
+// Github: davidpeet8
+var elements;
+var goodURIs = [
+    ["stackoverflow.com", "#ffe9a8"], 
+    ["www.w3schools.com", "#caffa8"], 
+    ["developer.mozilla.org", "#bccaff"]];
+
+// returns if str2 is found in str1
+function contains(str1, str2){
+    let curChar = 0;
+    for (let i = 0; i < str1.length; i++){
+        if(curChar == str2.length) return true;
+        else if(str1[i] == str2[curChar]){
+            curChar++;
+        }else curChar = 0;
+    }
+    return false;
+}
+
+function getElements(){
+    elements = document.getElementsByClassName('g');
+    console.log(elements);
+    console.log(elements[1].children[0].children[0].children[0].children[0].attributes[0].textContent);
+    for(let i = 1; i < elements.length; i++){
+        console.log(i);
+        if(elements[i].attributes[0].textContent == 'g'){
+            for(let j = 0; j < goodURIs.length; j++){
+                if(contains(elements[i].children[0].children[0].children[0].children[0].attributes[0].textContent, 
+                    goodURIs[j][0])){
+                    elements[i].style.padding = "15px";
+                    elements[i].style.borderRadius = "10px";
+                    elements[i].style.marginTop = "-10px";
+                    elements[i].style.background = goodURIs[j][1];
+                }
+            }
+        }
+    }
+}
+
+window.onload = getElements();
+
